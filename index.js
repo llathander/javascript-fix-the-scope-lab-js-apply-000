0ca3f96a6cf35f0df64cf9cfe9738161009b49e8
Adrian Samaniego <llathander@gmail.com>
2019-01-13 00:53:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,10 +24,14 @@ function add2(n) {
 
 var funkyFunction = function() {
   return function() {
-    return "FUNKY!"
+    return "FUNKY!";
   }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction
+
+
+
+// i tried to use the absolute value method to solve the math part :).....such craziness, when the solution was so simple!
